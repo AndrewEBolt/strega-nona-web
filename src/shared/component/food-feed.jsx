8ace6900dbaaded5994b2f08a53573c25a604c7c
Andrew Bolt <andrewebolt@gmail.com>
2017-07-16 23:52:03 -0700

    updating food feed grid to show placeholder image

--- src/shared/component/food-feed.jsx
+++ src/shared/component/food-feed.jsx
@@ -8,6 +8,7 @@ type FoodFeedProps = {
 	fetchFoodFeed: Function,
 	posts: Array<{
 		_id: string,
+		location: string,
 		picture: string,
 	}>,
 }
@@ -20,15 +21,11 @@ export default class FoodFeed extends React.Component {
 	props: FoodFeedProps
 
 	render() {
-		// eslint-disable-next-line no-console
-		console.log('render method of FoodPost component')
-		// eslint-disable-next-line no-console
-		console.log(this.props.posts)
 		// eslint-disable-next-line no-underscore-dangle
 		const displayPost = post => <FoodPost {...post} key={post._id} />
 
 		return (
-			<div className="clearfix col">
+			<div className="clearfix col col-12 mt3">
 				{this.props.posts && this.props.posts.map(displayPost)}
 			</div>
 		)
--- src/shared/component/food-post.jsx
+++ src/shared/component/food-post.jsx
@@ -1,18 +1,37 @@
 // @flow
 
+import styled from 'styled-components'
 import React from 'react'
+import { MdLocationOn } from 'react-icons/lib/md'
+
+import IconMd from './icon-md'
+
+const Picture = styled.picture`
+	background-color: gray;
+	display: block;
+	height: 220px;
+	width: 100%;
+`
+
+const Image = styled.div`
+	background-image: url(${props => props.picture});
+	height: 220px;
+	width: 100%;
+`
 
 type Props = {
+	location: string,
 	picture: string,
 }
 
-const FoodPost = ({ picture }: Props) => (
-	<div className="relative col col-12 lg-col-3 md-col-3 sm-col-12 my1 mb2 px2 overflow-hidden">
-		<picture>
-			<img alt="Food!" src={picture} />
-		</picture>
-		<div className="absolute left-0 right-0 bottom-0 bg-white">
-			Sweet placeholder text
+const FoodPost = ({ location, picture }: Props) => (
+	<div className="relative col col-12 lg-col-4 md-col-4 sm-col-12 my1 mb2 px2 overflow-hidden">
+		<Picture>
+			<Image picture={picture} />
+		</Picture>
+		<div className="absolute px2 left-0 right-0 bottom-0 bg-white">
+			<IconMd><MdLocationOn /></IconMd>
+			<span>{location}</span>
 		</div>
 	</div>
 )
